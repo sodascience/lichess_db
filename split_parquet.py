348e8df1171665d244d98d182575442ed5405692
maartenschermer <m.d.schermer@uu.nl>
2024-03-06 17:27:29 +0100

    refactor

--- split_parquet.py
+++ split_parquet.py
@@ -1,121 +1,115 @@
+"""Module to split Lichess Parquet files."""
 import argparse
 import glob
+import logging
 import pandas as pd
 from pathlib import Path
 
-class SplitParquet:
+logger=logging.getLogger()
 
-    def __init__(self, source, target, force_overwrite=False) -> None:
+def split_parquet(source: str, target: str, mode: str) -> None:
+    """Split existing Parquet files with Lichess chess games into folders and files.
 
-        if Path(source).is_file():
-            self.pfiles=[Path(source)]
-        else:
-            self.pfiles=glob.glob(glob.escape(source) + "*.parquet")
-        self.target=Path(target)
+    Parameters:
+        source (str): folder containing source Parquet files.
+        target (str): root folder of resulting folders with Parquet files.
 
-        self.force_overwrite=force_overwrite
+    The function will create folders based on Elo-rating buckets, and new Parquet files based
+    on games' year and month of play. This restructuring is intended to allow easier typical 
+    queries (typically through Apache Drill).
+    """
+    if Path(source).is_file():
+        pfiles=[Path(source)]
+    else:
+        pfiles=glob.glob(glob.escape(source) + "*.parquet")
 
-        print(f"found {len(self.pfiles)} files in '{source}'")
-        print(f"outputting to '{self.target}'")
+    logger.info(f"Found {len(pfiles)} files in '{source}'")
 
-    @staticmethod
-    def bin_elo(elo):
-        if elo<700:
-            return '0-700'
-        elif elo>=3000:
-            return '3000-inf'
-        else:
-            lower=(elo//100)*100
-            return f"{lower}-{lower+100}"
+    for pfile in pfiles:
+        _split_file(target=Path(target), pfile=pfile, mode=mode)
 
-    @staticmethod
-    def calc_game_type(time_control):
-        min, inc=time_control.split('+', 1)
-        sec=(int(min)*60)+(int(inc)*40)
-        if sec < 29:
-            return 'ultrabullet'
-        if sec < 179:
-            return 'bullet'
-        if sec < 479:
-            return 'blitz'
-        if sec < 1499:
-            return 'rapid'
-        return 'classical'
+def _bin_elo(elo):
+    if elo<700:
+        return '0-700'
+    if elo>=3000:
+        return '3000-inf'
+    lower=(elo//100)*100
+    return f"{lower}-{lower+100}"
 
-    def get_parquet_path(self, elo_bin, year_month):
-        # folder=self.target / Path(elo_bin) / Path(game_type)
-        folder=self.target / Path(elo_bin)
-        pfile=f"{year_month}.parquet"
-        folder.mkdir(parents=True, exist_ok=True)
-        return folder / pfile
+def _get_parquet_path(target, elo_bin, year_month):
+    folder=target / Path(elo_bin)
+    pfile=f"{year_month}.parquet"
+    folder.mkdir(parents=True, exist_ok=True)
+    return folder / pfile
 
-    def split(self):
-        for pfile in self.pfiles:
-            self.split_file(pfile)
+def _split_file(pfile, target, mode):
+    logger.info(f"Splitting '{pfile}'")
 
-    def split_file(self, pfile):
-        print(f"splitting {pfile}")
-        skipped=0
-        matches=pd.read_parquet(pfile, engine='pyarrow')
+    # read source file
+    matches = pd.read_parquet(pfile, engine='pyarrow')
 
-        dfs={}
-        for key, row in matches.iterrows():
-            if row['White'] is None or row['Black'] is None:
-                skipped+=1
-                continue
+    skipped = 0
+    dfs = {}
+    # iterate games
+    for key, row in matches.iterrows():
+        if row['White'] is None or row['Black'] is None:
+            skipped += 1
+            continue
+        
+        # determine Elo-bin
+        elo_bin = _bin_elo(int(min(row['WhiteElo'], row['BlackElo'])))
+        # get datestring
+        year_month = row['UTCDate'].strftime('%Y-%m')
 
-            elo_bin=self.bin_elo(int(min(row['WhiteElo'], row['BlackElo'])))
+        if not elo_bin in dfs:
+            dfs[elo_bin]={}
 
-            year_month=row['UTCDate'].strftime('%Y-%m')
+        if not year_month in dfs[elo_bin]:
+            dfs[elo_bin][year_month]=[]
 
-            if not elo_bin in dfs:
-                dfs[elo_bin]={}
+        # append game to appropriate bucket
+        dfs[elo_bin][year_month].append(row)
 
-            if not year_month in dfs[elo_bin]:
-                dfs[elo_bin][year_month]=[]
+        if key>0 and key%100000==0:
+            logger.debug(f"{key}")
 
-            # dfs[elo_bin][game_type].append(row)
-            dfs[elo_bin][year_month].append(row)
+    # saving games
+    for bin_elo, year_months in dfs.items():
+        for year_month, games in year_months.items():
+            parquet_path=_get_parquet_path(
+                target=target,
+                elo_bin=bin_elo,
+                year_month=year_month)
 
-            if key>0 and key%100000==0:
-                print(f"{key}")
+            logger.info(f"{bin_elo} {year_month} --> {parquet_path} ({len(games)})")
 
-        # for bin_elo, game_types in dfs.items():
-        for bin_elo, year_months in dfs.items():
-            for year_month, games in year_months.items():
-                parquet_path=self.get_parquet_path(
-                    elo_bin=bin_elo,
-                    year_month=year_month)
+            new_df = pd.DataFrame(games).astype({"UTCTime":str})
 
-                print(f"{bin_elo} {year_month} {parquet_path} ({len(games)})")
+            if Path(parquet_path).is_file():
+                if mode=='append':
+                    new_df = pd.concat([pd.read_parquet(parquet_path, engine='pyarrow'), new_df])
+                Path.unlink(parquet_path)
+            
+            new_df.astype({"UTCTime":str}).to_parquet(
+                path=parquet_path,
+                compression='gzip',
+                engine='pyarrow',
+                index=False)
 
-                if self.force_overwrite and Path(parquet_path).is_file():
-                    Path.unlink(parquet_path)
-                    print(f"deleted existing '{parquet_path}'")
-
-                if not Path(parquet_path).is_file():
-                    (pd.DataFrame(games).
-                        astype({"UTCTime":str}).
-                        to_parquet(
-                            path=parquet_path, 
-                            compression='gzip', 
-                            engine='pyarrow',
-                            index=False))
-                else:
-                    print(f"file exists '{parquet_path}'")
-
-        print(f"skipped {skipped}")
+    logger.info(f"Skipped {skipped} games (black or white missing)")
 
 if __name__=="__main__":
-    
 
     parser=argparse.ArgumentParser()
     parser.add_argument('-i','--input-path', required=True)
     parser.add_argument('-o','--output-path', required=True)
-    parser.add_argument('-f','--force-overwrite', action='store_true', default=False)
+    parser.add_argument('--mode', choices=['overwrite', 'append'], default='append')
+    parser.add_argument('--debug', action='store_true')
     args=parser.parse_args()
-    
-    spf=SplitParquet(source=args.input_path, 
-                     target=args.output_path, 
-                     force_overwrite=args.force_overwrite)
-    spf.split()
+
+    logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
+
+    split_parquet(
+        source=args.input_path,
+        target=args.output_path,
+        mode=args.mode)
